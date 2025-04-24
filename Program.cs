e9c9b6833d39b64007ae4907bd598e3496714331
Demetrio M. Junior <71155627+demetriomjr@users.noreply.github.com>
2025-04-24 10:58:05 -0300

    Analitic commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.Run();
