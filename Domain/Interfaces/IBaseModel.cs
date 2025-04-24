bb64e6cad755a399cacc03b916f4f38c9507754a
Demetrio M. Junior <71155627+demetriomjr@users.noreply.github.com>
2025-04-24 10:55:49 -0300

    Initialized project

---
+++ Domain/Interfaces/IBaseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OWF.API.Domain.Interfaces;
+
+public interface IBaseModel
+{
+    public enum ModelState
+    {
+        DELETED = -1,
+        INACTIVE = 0,
+        ACTIVE = 1,
+    }
+    
+    public long Id { get; set; }
+    public ModelState State { get; set; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public DateTimeOffset DeletedAt { get; set; }
+}
---
+++ Domain/User/User.cs
@@ -0,0 +1,22 @@
+﻿using OWF.API.Domain.Interfaces;
+
+namespace OWF.API.Domain.User;
+
+public class User : IBaseModel, ICloneable
+{
+    public long Id { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+    public string Name { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public byte[] Password { get; set; } = [];
+    
+    public IBaseModel.ModelState State { get; set; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public DateTimeOffset DeletedAt { get; set; }
+    
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+}
